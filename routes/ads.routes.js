ffcf7bf3050d759c7485d470f07c4c300554d8e4
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-01-30 10:24:57 +0100

    add changes

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -6,8 +6,8 @@ const upload = multer({ dest: 'uploads/'})
 const ads = require('../controller/ads.controller');
 
 router.get('/ads', ads.load);
-router.post('/ads', ads.add);
+router.post('/ads', upload.single('photo'),ads.add);
 router.put('/ads/:id', ads.edit);
-router.post('./ads', upload.single('photo'));
+
 
 module.exports = router;
