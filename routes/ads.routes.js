41eb41f85fd1c8a39cb02f9d1d64bcae1c7222c7
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-01-30 10:19:18 +0100

    add changes

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/'})
 
 const ads = require('../controller/ads.controller');
 
